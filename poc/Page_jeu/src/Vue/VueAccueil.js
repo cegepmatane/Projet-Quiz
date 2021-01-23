90251ade92beee54bb934e5742bed0fab1338e59
KennyMarechal <44245153+KennyMarechal@users.noreply.github.com>
2021-01-23 14:49:35 -0500

    Intégration de la vue Jeu #WIP
    Intégration de la maquette en JSX pour React

--- poc/Page_jeu/src/Vue/VueAccueil.js
+++ poc/Page_jeu/src/Vue/VueAccueil.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import Logo from '../logo.svg';
 import '../Decoration/VueAccueil.css';
+import LogoQuiz from '../logo-quiz.png';
 
 function VueAccueil(){
     return(
         <div className="vue-accueil-html">
             <div className="titre">
                 <h1>Le Quiz des Quiz</h1>
-                <img src="https://place-hold.it/100x80" className="logo"/>
             </div>
-                <div className="contenue">
+            <img src={LogoQuiz} className="logo"/>
+
+            <div className="contenue">
                 <div className="contenue-chat">
-                    
+                    <div className="chat"></div>
                 </div>
                 <div className="contenue-jeu">
-                    
+                    <div className="contenue-jeu-question">
+                        <div className="question">
+                            <h2>Voici une question?</h2>
+                        </div>
+                        <div className="contenue-choix-reponse">
+                            <a href="#" className="reponse-1"> Réponse 1</a>
+                            <a href="#" className="reponse-2"> Réponse 2</a>
+                            <a href="#" className="reponse-3"> Réponse 3</a>
+                            <a href="#" className="reponse-4"> Réponse 4</a>
+                        </div>
+                    </div>
+                    <a href="#" className="quiter-partie">Quiter la partie</a>
                 </div>
                 <div className="contenue-info">
-                    
+                <div className="info"></div> 
                 </div>
             </div>
         </div>
