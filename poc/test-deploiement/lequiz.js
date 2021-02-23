aa689536eb0f666f37d2306b4b45a97e257b6410
Simon Delarue <51883276+simondelarue14@users.noreply.github.com>
2021-02-23 17:21:58 -0500

    #11 Application mise en ligne

---
+++ poc/test-deploiement/lequiz.js
@@ -0,0 +1,40 @@
+﻿/*const express = require('express');
+const path = require('path');
+const app = express();
+const https = require('https');
+const fs = require('fs');
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3000, function () {
+    console.log('Serveur allumé sur le port 3000');
+})
+
+https.createServer({
+        key: fs.readFileSync('./server.key'),
+        cert: fs.readFileSync('./server.crt'),
+        passphrase: 'motdepasseSSL#'
+},
+    app).listen(3000, function () {
+    console.log('Serveur allumé sur le port 3000');
+});*/
+
+
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3000);
